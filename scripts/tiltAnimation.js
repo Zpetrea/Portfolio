6694639ee5fa65ced65437a0859418e381bd2292
Zachary Petrea <93063718+Zpetrea@users.noreply.github.com>
2025-05-27 11:27:40 -0400

    Update tiltAnimation.js

--- scripts/tiltAnimation.js
+++ scripts/tiltAnimation.js
@@ -1,6 +1,13 @@
-import VanillaTilt from "vanilla-tilt";
+export default function initTiltEffect() {
+  if (typeof VanillaTilt === 'undefined') {
+    console.warn("VanillaTilt is not loaded.");
+    return;
+  }
 
-export default function initTiltAnimation() {
-  const elements = document.querySelectorAll(".js-tilt");
-  VanillaTilt.init(elements);
+  VanillaTilt.init(document.querySelectorAll(".js-tilt"), {
+    max: 4,
+    speed: 400,
+    glare: true,
+    "max-glare": 0.5,
+  });
 }
