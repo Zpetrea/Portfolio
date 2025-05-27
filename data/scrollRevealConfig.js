8a918eb9d3b7ca00255d5a4cf7d6f10d1aba4022
Zachary Petrea <93063718+Zpetrea@users.noreply.github.com>
2025-05-27 12:47:00 -0400

    Add files via upload

---
+++ data/scrollRevealConfig.js
@@ -0,0 +1,8 @@
+export const targetElements = [".hero-title"];
+export const defaultProps = {
+  distance: "30px",
+  duration: 1000,
+  easing: "ease-out",
+  origin: "bottom",
+  interval: 100,
+};
