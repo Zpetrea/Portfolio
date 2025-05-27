dea8a93ad784f7793709fe3d743b03f9a593d3ea
Zachary Petrea <93063718+Zpetrea@users.noreply.github.com>
2025-05-27 12:52:55 -0400

    Delete data directory

--- data/scrollRevealConfig.js
+++
@@ -1,8 +0,0 @@
-export const targetElements = [".hero-title"];
-export const defaultProps = {
-  distance: "30px",
-  duration: 1000,
-  easing: "ease-out",
-  origin: "bottom",
-  interval: 100,
-};
